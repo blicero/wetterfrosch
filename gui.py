3226e2ab586b771b58671ae466c7d2cb01d631c6
Benjamin Walkenhorst <krylon@posteo.de>
2024-01-06 20:22:17 +0100

    Render timestamps properly, display notifications
    Also, optionally read warnings from file, which is mainly for testing
    without having to wait for some warning-worthy weather event to occur.

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2024-01-05 18:33:39 krylon>
+# Time-stamp: <2024-01-06 20:20:10 krylon>
 #
 # /data/code/python/wetterfrosch/gui.py
 # created on 02. 01. 2024
@@ -16,18 +16,25 @@ wetterfrosch.gui
 (c) 2024 Benjamin Walkenhorst
 """
 
+import json
+from datetime import datetime
 from threading import Lock, local
 from typing import Any, Final
 
+import notify2
+
 import gi  # type: ignore
 
-from wetterfrosch import common, client
+from wetterfrosch import client, common
 
 gi.require_version("Gtk", "3.0")
 gi.require_version("Gdk", "3.0")
+gi.require_version("GLib", "2.0")
 
 from gi.repository import \
     Gtk as gtk  # noqa: E402 pylint: disable-msg=C0413,C0411
+from gi.repository import GLib \
+    as glib  # noqa: E402 pylint: disable-msg=C0413,C0411
 
 
 # pylint: disable-msg=R0902,R0903
@@ -35,6 +42,7 @@ class WetterGUI:
     """Graphical frontend to the wetterfrosch app"""
 
     def __init__(self) -> None:
+        notify2.init(common.APP_NAME, "glib")
         self.log = common.get_logger("GUI")
         self.lock = Final[Lock]
         self.local = local()
@@ -60,8 +68,8 @@ class WetterGUI:
             int,  # Record ID
             int,  # Level
             str,  # Region
-            int,  # Start
-            int,  # End
+            str,  # Start
+            str,  # End
             str,  # Event
             str,  # Headline
             str,  # Description
@@ -80,6 +88,8 @@ class WetterGUI:
             gtk.MenuItem.new_with_mnemonic("_Aktualisieren")
         self.fm_quit_item: gtk.MenuItem = \
             gtk.MenuItem.new_with_mnemonic("_Beenden")
+        self.fm_load_item: gtk.MenuItem = \
+            gtk.MenuItem.new_with_mnemonic("_Lade aus Datei...")
 
         self.warn_view = gtk.TreeView(model=self.store)
 
@@ -88,7 +98,7 @@ class WetterGUI:
                 c[1],
                 gtk.CellRendererText(),
                 text=c[0],
-                weight=1,
+                size=12,
             )
             self.warn_view.append_column(col)
 
@@ -112,6 +122,8 @@ class WetterGUI:
         self.menubar.add(self.mb_file_item)
         self.mb_file_item.set_submenu(self.file_menu)
         self.file_menu.add(self.fm_refresh_item)
+        if common.DEBUG:
+            self.file_menu.add(self.fm_load_item)
         self.file_menu.add(self.fm_quit_item)
 
         ################################################################
@@ -121,8 +133,11 @@ class WetterGUI:
         self.win.connect("destroy", self.__quit)
         self.fm_quit_item.connect("activate", self.__quit)
         self.fm_refresh_item.connect("activate", self.load)
+        if common.DEBUG:
+            self.fm_load_item.connect("activate", self.load_from_file)
 
         self.win.show_all()
+        glib.timeout_add(300_000, self.load)
 
     def __quit(self, *_ignore: Any) -> None:
         self.win.destroy()
@@ -153,34 +168,83 @@ class WetterGUI:
         finally:
             dlg.destroy()
 
-    def load(self, _ignore: Any) -> None:
-        """Fetch data, process, display"""
-        raw = self.client.fetch()
-        if raw is None:
-            self.display_msg("Client did not return any data.")
-            return
-        proc = self.client.process(raw)
-        if proc is None:
-            self.display_msg("No warnings were left after processing.")
-            return
+    def display_data(self, data: list[dict]) -> None:
+        """Display weather warnings."""
         self.store.clear()
-        for event in proc:
+        now: Final[datetime] = datetime.now()
+        for event in data:
+            d1: Final[datetime] = datetime.fromtimestamp(event["start"]/1000)
+            d2: Final[datetime] = datetime.fromtimestamp(event["end"]/1000)
+
+            if d1 <= now <= d2:
+                n = notify2.Notification(
+                    event["headline"],
+                    event["description"])
+                n.show()
+
             liter = self.store.append()
             self.store.set(
                 liter,
-                (0, 1, 2, 3, 4, 5, 6, 7),
+                (0, 1, 2, 3, 4, 5, 6, 7, 8),
                 (
                     0,
                     event["level"],
                     event["regionName"],
-                    event["start"],
-                    event["end"],
+                    d1.strftime(common.TIME_FMT),
+                    d2.strftime(common.TIME_FMT),
                     event["event"],
                     event["headline"],
                     event["description"],
-                    event["instructions"],
+                    event["instruction"],
                 ),
             )
+
+    def load(self, *_ignore: Any) -> bool:
+        """Fetch data, process, display"""
+        try:
+            raw = self.client.fetch()
+            if raw is None:
+                self.display_msg("Client did not return any data.")
+                return True
+            proc = self.client.process(raw)
+            if proc is None or len(proc) == 0:
+                self.display_msg("No warnings were left after processing.")
+                return True
+            self.display_data(proc)
+            return True
+        except Exception as e:  # pylint: disable-msg=W0718
+            self.log.error("Something went wrong refreshing our data: %s", e)
+            return True
+
+    def load_from_file(self, _ignore: Any) -> None:
+        """Load warnings from a file, mainly for testing purposes."""
+        dlg: gtk.FileChooserDialog = gtk.FileChooserDialog(
+            title="Datei auswählen...",
+            parent=self.win,
+            action=gtk.FileChooserAction.OPEN)
+        dlg.add_buttons(
+            gtk.STOCK_CANCEL,
+            gtk.ResponseType.CANCEL,
+            gtk.STOCK_OPEN,
+            gtk.ResponseType.OK,
+        )
+
+        try:
+            res = dlg.run()
+            if res != gtk.ResponseType.OK:
+                self.log.debug("Response from FileChooserDialog was {res}")
+                return
+
+            path: Final[str] = dlg.get_filename()
+            self.log.debug("Read warnings from {path}")
+
+            with open(path, "r", encoding="utf-8") as fh:
+                data = json.load(fh)
+                warnings = self.client.process(data)
+                assert warnings is not None
+                self.display_data(warnings)
+        finally:
+            dlg.destroy()
 
 
 def main() -> None:
